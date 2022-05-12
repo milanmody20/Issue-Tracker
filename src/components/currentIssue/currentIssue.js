77df639334347a5e2b9b60744ecbc984a45bd21e
Milan <milanm1120@gmail.com>
2022-05-12 12:24:38 -0400

    adjust Current Issues card height and allow for scroll functionality

--- src/components/currentIssue/currentIssue.js
+++ src/components/currentIssue/currentIssue.js
@@ -19,6 +19,48 @@ function CurrentIssue() {
         </p>
         <hr/>
       </div>
+
+      <div className= "indv-issue">
+        <p className="close-issue">Close Issue</p>
+        <p>Assigned<span>Username</span></p>
+        <p>Priority<span>High</span></p>
+        <p>Description</p>
+        <p className= "description">
+          A short description of the project describing the issue. 
+          Line of text can take up 2-3 lines. Users will enter the needed 
+          issue which needs to be fixed. This is dumby text which is hard
+          coded at this time. 
+        </p>
+        <hr/>
+      </div>
+
+      <div className= "indv-issue">
+        <p className="close-issue">Close Issue</p>
+        <p>Assigned<span>Username</span></p>
+        <p>Priority<span>High</span></p>
+        <p>Description</p>
+        <p className= "description">
+          A short description of the project describing the issue. 
+          Line of text can take up 2-3 lines. Users will enter the needed 
+          issue which needs to be fixed. This is dumby text which is hard
+          coded at this time. 
+        </p>
+        <hr/>
+      </div>
+
+      <div className= "indv-issue">
+        <p className="close-issue">Close Issue</p>
+        <p>Assigned<span>Username</span></p>
+        <p>Priority<span>High</span></p>
+        <p>Description</p>
+        <p className= "description">
+          A short description of the project describing the issue. 
+          Line of text can take up 2-3 lines. Users will enter the needed 
+          issue which needs to be fixed. This is dumby text which is hard
+          coded at this time. 
+        </p>
+        <hr/>
+      </div>
     </div>
   );
 }
